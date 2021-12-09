4922a8979742323009c08a4aa024d74b54ddeef3
Snake5932 <shtjnk@yandex.ru>
2021-12-10 00:51:46 +0300

    Scripted auto-commit on change (2021-12-10 00:51:46) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab5/StressTest.java
+++ src/main/java/ru/bmstu/iu9/lab5/StressTest.java
@@ -1,8 +1,12 @@
 package ru.bmstu.iu9.lab5;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
 
@@ -13,6 +17,6 @@ public class StressTest {
         ActorSystem system = ActorSystem.create(AKKA_SYSTEM_NAME);
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
     }
 }
