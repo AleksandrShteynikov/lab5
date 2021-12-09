670e0050f47065282a2ad68f842b3bf584ccd65c
Snake5932 <shtjnk@yandex.ru>
2021-12-10 00:50:24 +0300

    Scripted auto-commit on change (2021-12-10 00:50:24) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab5/StressTest.java
+++ src/main/java/ru/bmstu/iu9/lab5/StressTest.java
@@ -1,6 +1,8 @@
 package ru.bmstu.iu9.lab5;
 
 import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 import java.io.IOException;
 
@@ -9,6 +11,8 @@ public class StressTest {
 
     public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create(AKKA_SYSTEM_NAME);
-        
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow
     }
 }
