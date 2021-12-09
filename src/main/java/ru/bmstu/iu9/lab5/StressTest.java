234a5cf98b228ec7c224f53cf8f51bedfa06c301
Snake5932 <shtjnk@yandex.ru>
2021-12-10 00:27:55 +0300

    Scripted auto-commit on change (2021-12-10 00:27:55) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab5/StressTest.java
+++ src/main/java/ru/bmstu/iu9/lab5/StressTest.java
@@ -1,7 +1,7 @@
 package ru.bmstu.iu9.lab5;
 
 public class StressTest {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+
     }
 }
