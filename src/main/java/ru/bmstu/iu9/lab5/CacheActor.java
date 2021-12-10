6e897685e2727af4be160954364899c1a46edc9d
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-10 11:19:12 +0300

    Scripted auto-commit on change (2021-12-10 11:19:12) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab5/CacheActor.java
+++ src/main/java/ru/bmstu/iu9/lab5/CacheActor.java
@@ -1,10 +1,24 @@
 package ru.bmstu.iu9.lab5;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class CacheActor extends AbstractActor {
+    
+    private final Map<String, Long> results = new HashMap<>();
+
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(String.class, req -> getSender()
+                                            .tell(Optional.of(results.get(req)), self()))
+                .match(Result.class, res -> {
+                    results.put(res.getUrl(), res.getTime());
+                })
+                .build();
     }
 }
--- src/main/java/ru/bmstu/iu9/lab5/Result.java
+++ src/main/java/ru/bmstu/iu9/lab5/Result.java
@@ -1,4 +1,19 @@
 package ru.bmstu.iu9.lab5;
 
 public class Result {
+    private final String url;
+    private final Long time;
+
+    public Result(String url, Long time) {
+        this.url = url;
+        this.time = time;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Long getTime() {
+        return time;
+    }
 }
